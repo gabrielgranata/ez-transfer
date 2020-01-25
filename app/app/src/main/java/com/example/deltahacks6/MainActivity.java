21da5948a0790e74ff10029907c76307b67243d2
Ruben Plaza <ruben.d.plaza@ryerson.ca>
2020-01-25 18:38:20 -0500

    initial front end implementation

--- app/app/src/main/java/com/example/deltahacks6/MainActivity.java
+++ app/app/src/main/java/com/example/deltahacks6/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.deltahacks6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView fromTextView;
+    TextView toTextView;
+    TextView amountTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fromTextView = findViewById(R.id.sendFromAddress);
+        String fromAddress = fromTextView.getText().toString();
+
+        toTextView = findViewById(R.id.sendToAddress);
+        String toAddress = toTextView.getText().toString();
+
+        amountTextView = findViewById(R.id.amount);
+        int amount =  Integer.parseInt(amountTextView.getText().toString());
+    }
+
+    public void onSendBtnPress(View v){
+
+        String fromAddress = fromTextView.getText().toString();
+        String toAddress = toTextView.getText().toString();
+        int amount =  Integer.parseInt(amountTextView.getText().toString());
+
+
+
     }
 }
