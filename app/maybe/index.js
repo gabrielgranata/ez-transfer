cefda03df26648e62690a8f8e3baaa92de9d8bc5
gabrielgranata <ggran067@uottawa.ca>
2020-01-26 11:08:08 -0500

    node server

---
+++ app/maybe/index.js
@@ -0,0 +1,24 @@
+let express = require('express');
+
+let app = express();
+
+let algosdk = require('algosdk');
+
+
+app.get('/', function() {
+    console.log("Hello World");
+})
+
+createAccount();
+
+function createAccount() {
+    let account = algosdk.generateAccount();
+    let address = account.addr;
+    let sk = account.sk;
+    let mnemonic = algosdk.secretKeyToMnemonic(sk);
+
+    console.log(address);
+    console.log(mnemonic);
+
+    
+}
